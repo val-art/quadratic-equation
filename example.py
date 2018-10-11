1fa5d1e0c2fcb714c8c5ad1ede04219078164605
Valentin.Rybin@artezio.com <Valentin.Rybin@artezio.com>
2018-10-11 18:57:30 +0300

    Style changes

--- example.py
+++ example.py
@@ -1,7 +1,6 @@
 from cmath import sqrt
 import sys
 
-
 # a*x^2 + b*x + c = 0
 a = int(sys.argv[1])
 b = int(sys.argv[2])
@@ -9,8 +8,7 @@ c = int(sys.argv[3])
 
 D = b ** 2 - 4 * a * c
 
-
 x1 = (-b + sqrt(D)) / (2.0 * a)
 x2 = (-b - sqrt(D)) / (2.0 * a)
-print "x1 = {}".format(x1)
-print "x2 = {}".format(x2)
+print "X1 = {}".format(x1)
+print "X2 = {}".format(x2)
