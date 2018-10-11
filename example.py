863e5c4431a2316838d669cdc1bfe9f4d7a669f4
Valentin <Valentin.Rybin@artezio.com>
2018-10-11 19:29:02 +0300

    Print answer on one line

--- example.py
+++ example.py
@@ -6,9 +6,10 @@ a = int(sys.argv[1])
 b = int(sys.argv[2])
 c = int(sys.argv[3])
 
+# Some comment
+
 D = b ** 2 - 4 * a * c
 
 x1 = (-b + sqrt(D)) / (2.0 * a)
 x2 = (-b - sqrt(D)) / (2.0 * a)
-print "X1 = {}".format(x1)
-print "X2 = {}".format(x2)
+print "X1 = {}, X2 = {}".format(x1, x2)
